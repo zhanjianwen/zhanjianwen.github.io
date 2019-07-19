7673b1466a4d2f9d88326fe0294e824563c8541e
zhanjianwen <zhanjianwen163@163.com>
2019-07-19 14:45:56 +0800

    修改

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
+import { Button } from 'antd';
 import './App.less';
-
 import logo from './logo.svg';
 
 class App extends React.Component {
@@ -14,6 +14,7 @@ class App extends React.Component {
         <p className="App-intro">
           To get started, edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <Button type="primary">Primary</Button>
       </div>
     );
   }
--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,10 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import 'antd/dist/antd.css';
 import registerServiceWorker from './registerServiceWorker';
-
+// import * as moment from 'moment';
+// import * as _ from 'lodash';
 ReactDOM.render(
   <App />,
   document.getElementById('root') as HTMLElement
