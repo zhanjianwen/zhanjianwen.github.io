12e65d7e05d519031a7c4768eed00ba3c86cc956
zhanjianwen <zhanjianwen163@163.com>
2019-08-06 10:03:44 +0800

    修改

--- src/views/components/NotFound/index.tsx
+++ src/views/components/NotFound/index.tsx
@@ -1,29 +1,19 @@
-// import * as React from 'react'
-// class NotFound extends React.Component {
-//     render() {
-//         return (
-//             <div>404</div>
-//         );
-//     }
-// }
-// export default NotFound
 import * as React from 'react'
 import img from '../../../statics/images/404.png';
-
 import './index.less'
 class NotFound extends React.Component {
-    state = {
-        animated: ''
-    };
-    enter = () => {
-        this.setState({ animated: 'hinge' })
-    };
-    render() {
-        return (
-            <div className="notFound" style={{ height: '100%', background: '#ececec', overflow: 'hidden' }}>
-                <img src={img} alt="404" className={`animated swing ${this.state.animated}`} onMouseEnter={this.enter} />
-            </div>
-        )
-    }
+  state = {
+    animated: ''
+  };
+  enter = () => {
+    this.setState({ animated: 'hinge' })
+  };
+  render() {
+    return (
+      <div className="notFound" style={{ height: '100%', background: '#ececec', overflow: 'hidden' }}>
+        <img src={img} alt="404" className={`animated swing ${this.state.animated}`} onMouseEnter={this.enter} />
+      </div>
+    )
+  }
 }
 export default NotFound
